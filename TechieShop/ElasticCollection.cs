1d50820d21f1049b4919c7bdb4ff20360f227c6c
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-01-18 21:59:13 +0100

    Add project files.

---
+++ TechieShop/ElasticCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace TechieShop
+{
+    class ElasticCollection
+    {
+        public List<ExpandoObject> Products { get; set; }
+
+        public ElasticCollection()
+        {
+            Products = new List<ExpandoObject>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            dynamic productObject = new ExpandoObject();
+            productObject.Id = product.Id;
+            productObject.Name = product.Name;
+            productObject.Quantity = product.Quantity;
+            productObject.Price = product.Price;
+            productObject.Category = product.Category;
+            productObject.OrderCount = product.OrderCount;
+
+            Products.Add(productObject);
+        }
+
+        public void Display(List<string> properties = null)
+        {
+            if (properties == null)
+            {
+                properties = new List<string>() { "Id", "Name", "Quantity", "Price", "Category", "OrderCount" };
+            }
+
+            foreach (var product in Products)
+            {
+                foreach (var property in properties)
+                {
+                    if (((IDictionary<string, object>)product).ContainsKey(property))
+                    {
+                        Console.Write(((IDictionary<string, object>)product)[property] + ", ");
+                    }
+                    else
+                    {
+                        Console.Write("property not exists, ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TechieShop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TechieShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElasticCollection elasticCollection = new ElasticCollection();
+
+            elasticCollection.AddProduct(new Product { Id = 1, Name = "Dell Xps", Quantity = 30, Price = 1500, Category = "PCs", OrderCount = 1000 });
+            elasticCollection.AddProduct(new Product { Id = 2, Name = "Ergonomic Chair", Quantity = 400, Price = 200, Category = "Chairs", OrderCount = 4000 });
+            elasticCollection.AddProduct(new Product { Id = 3, Name = "Table", Quantity = 500, Price = 250, Category = "Tables", OrderCount = 3000 });
+
+
+            Console.WriteLine("******************************\nWithout Specifying a category\n******************************\n");
+            elasticCollection.Display();
+
+            Console.WriteLine("\n******************************\nWith Specification a category\n******************************\n");
+            elasticCollection.Display(new List<string>() { "Name", "Quantity", "OrderCount", "Category" });
+
+        }
+    }
+}
